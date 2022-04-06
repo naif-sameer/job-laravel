cf6ec1f2aab94f06bb01185e6d35cf6e3405bad6
Naif <nayef7138@gmail.com>
2022-04-06 05:25:58 +0300

    update: sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Windmill Dashboard</title>
+  <title>
+    @yield('title')
+  </title>
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
   <style>
