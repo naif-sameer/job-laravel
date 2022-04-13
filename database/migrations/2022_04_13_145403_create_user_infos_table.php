a63fb2e3b5cdac458aab368b734ea5518169b0bc
Naif <nayef7138@gmail.com>
2022-04-13 20:32:05 +0300

    add migrations files

---
+++ database/migrations/2022_04_13_145403_create_user_infos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('user_infos', function (Blueprint $table) {
+      $table->id();
+      $table->string('phone');
+      $table->string('gender');
+      $table->string('birth_date');
+      $table->string('description');
+      $table->boolean('is_active')->default(1);
+
+      $table->foreignId('experience_id');
+      $table->foreign('experience_id')->references('id')->on('user_experiences')->onDelete('cascade');
+
+      $table->foreignId('course_id');
+      $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('user_infos');
+  }
+};
